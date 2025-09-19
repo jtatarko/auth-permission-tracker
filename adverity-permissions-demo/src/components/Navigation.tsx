71473121640ca1978c6117985f58403912c005ad
Jakub Tatarko <jtatarko@gmail.com>
2025-09-19 16:08:18 +0200

    Attempts to fix 404 deployment issue

--- adverity-permissions-demo/src/components/Navigation.tsx
+++ adverity-permissions-demo/src/components/Navigation.tsx
@@ -27,7 +27,7 @@ const Navigation: React.FC = () => {
       <CardContent className="p-2">
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-2 text-sm">
-            <img src="/logos/Adverity_Brandmark.svg" />
+            <img src="/logos/Adverity_Brandmark.svg" alt="Adverity" className="h-6 w-6" />
             {/* <Home className="h-5 w-5 text-blue-600" /> */}
             <span>Adverity Permissions Demo</span>
           </div>
